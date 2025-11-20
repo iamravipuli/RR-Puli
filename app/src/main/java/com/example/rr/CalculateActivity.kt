eb5fa67da6c8fe6c59d9d00b01cf0079c6622f6a
iamravipuli <sairavitejapuli@gmail.com>
2025-11-20 12:17:52 +0530

    Create CalculateActivity.kt

---
+++ app/src/main/java/com/example/rr/CalculateActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rrpuli
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CalculateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO: Add PULI-style interest calculator later
+    }
+}
