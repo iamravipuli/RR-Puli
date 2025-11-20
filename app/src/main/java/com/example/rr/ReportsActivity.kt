d47217eda645e540093da9dd86396cfaf2851129
iamravipuli <sairavitejapuli@gmail.com>
2025-11-20 12:17:07 +0530

    Create ReportsActivity.kt

---
+++ app/src/main/java/com/example/rr/ReportsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rrpuli
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReportsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO: Add Reports UI later
+    }
+}
