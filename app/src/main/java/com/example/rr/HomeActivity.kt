0588f451782e039758ffcda312d532504e4c75e4
iamravipuli <sairavitejapuli@gmail.com>
2025-11-21 16:16:24 +0530

    Update HomeActivity.kt

--- app/src/main/java/com/example/rr/HomeActivity.kt
+++ app/src/main/java/com/example/rr/HomeActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
+import android.view.View
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
